06e3174211221654c7d5c2c5eaf60a13788fef35
Çağan Seyrek <115536535+caganseyrek@users.noreply.github.com>
2023-12-25 23:43:21 +0300

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Frame {
+    public static JFrame frame = new JFrame();
+
+    // Work in progress
+
+    frame.setLayout(new BorderLayout(10, 5));
+    frame.setSize(1200, 750);
+    frame.setLocationRelativeTo(null);
+    frame.setTitle("Blackjack - Main Menu");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    JPanel background = new JPanel();
+    background.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+    background.setBackground(new ColorUIResource(53, 101, 77));
+
+    frame.add(background);
+    frame.setVisible(true);
+    Deck.getCardImage(Deck.generateDeck().get(0));
+}

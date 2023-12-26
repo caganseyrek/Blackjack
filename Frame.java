b3e32ca1aadce84f5e3e0158d876f0344c20b937
Çağan Seyrek <115536535+caganseyrek@users.noreply.github.com>
2023-12-26 21:11:48 +0300

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -1,22 +1,78 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.plaf.ColorUIResource;
 
 public class Frame {
-    public static JFrame frame = new JFrame();
-
-    // Work in progress - currently not working
     
-    frame.setLayout(new BorderLayout(10, 5));
-    frame.setSize(1200, 750);
-    frame.setLocationRelativeTo(null);
-    frame.setTitle("Blackjack - Main Menu");
-    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    // Work in progress
+    public static void setFrame() {
+        ArrayList<String> dealer = Game.dealerHand;
+        ArrayList<String> player = Game.playerHand;
 
-    JPanel background = new JPanel();
-    background.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
-    background.setBackground(new ColorUIResource(53, 101, 77));
+        JFrame frame = new JFrame("Blackjack");
+        JPanel background = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
 
-    frame.add(background);
-    frame.setVisible(true);
-    Deck.getCardImage(Deck.generateDeck().get(0));
+                int width = 110;
+                int height = 154;
+                int i = 0;
+
+                try {
+                    BufferedImage back = ImageIO.read(new File("Blackjack/cards/back.png"));
+                    g.drawImage(back, 20, 20, width, height, null);
+
+                    for (String card : dealer) {
+                        String path = Deck.getCardImage(card);
+                        BufferedImage cardImage = ImageIO.read(new File(path));
+                        g.drawImage(cardImage, (width + 25 + (width+5)*i), 20, width, height, null);
+                        i++;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        JPanel buttons = new JPanel();
+
+        JButton hit = new JButton("Hit");
+        JButton stay = new JButton("Stay");
+
+        frame.setVisible(true);
+        frame.setSize(1000, 625);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        background.setLayout(new BorderLayout());
+        background.setBackground(new ColorUIResource(53, 101, 77));
+
+        frame.add(background);
+
+        hit.setContentAreaFilled(false);
+        hit.setPreferredSize(new Dimension(80, 30));
+        hit.setFocusable(false);
+
+        stay.setContentAreaFilled(false);
+        stay.setPreferredSize(new Dimension(80, 30));
+        stay.setFocusable(false);
+
+        buttons.add(hit);
+        buttons.add(stay);
+
+        frame.add(buttons, BorderLayout.SOUTH);
+    }
 }

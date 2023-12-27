9a5b2a928bec87e13e885daa2a9716977c6c37ed
Çağan Seyrek <115536535+caganseyrek@users.noreply.github.com>
2023-12-27 13:46:58 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,94 +1,104 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Arrays;
 
-public class Deck {
-    public static String[] ranks = new String[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-    public static String[] suits = new String[] { "Hearts", "Diamonds", "Clubs", "Spades" };
-    public static HashMap<String, Integer> values = new HashMap<String, Integer>();
+public class Game {
+    public static boolean playerBust = false;
+    public static boolean dealerReveal = false;
 
-    static {
-        values.put("2", 2);
-        values.put("3", 3);
-        values.put("4", 4);
-        values.put("5", 5);
-        values.put("6", 6);
-        values.put("7", 7);
-        values.put("8", 8);
-        values.put("9", 9);
-        values.put("10", 10);
-        values.put("Jack", 10);
-        values.put("Queen", 10);
-        values.put("King", 10);
-        values.put("Ace", 11);
-    }
+    public static ArrayList<String> playerHand = new ArrayList<>();
+    public static ArrayList<String> dealerHand = new ArrayList<>();
 
-    public static ArrayList<String> generateDeck() {
-        ArrayList<String> newDeck = new ArrayList<String>();
-        for (String rank : ranks) {
-            for (String suit : suits) {
-                newDeck.add(rank + " of " + suit);
+    public static void startGame() {
+        resetGame();
+        Chips.newBet();
+
+        ArrayList<String> playDeck = Deck.generateDeck();
+
+        playerHand.add(Deck.drawCard(playDeck));
+        dealerHand.add(Deck.drawCard(playDeck)); // hidden card
+        playerHand.add(Deck.drawCard(playDeck));
+        dealerHand.add(Deck.drawCard(playDeck));
+
+        String hiddenCard = dealerHand.get(0);
+
+        while (true) {
+            if (Deck.getHandValue(playerHand) > 21) {
+                playerBust = true;
+                dealerReveal = true;
+                Deck.getStatus(dealerHand, playerHand, hiddenCard, dealerReveal);
+                System.out.println("Your hand went over 21. You lost.");
+                Chips.playerChips -= Chips.playerBet;
+                break;
+            } else if (Deck.getHandValue(playerHand) == 21) {
+                dealerReveal = true;
+                Deck.getStatus(dealerHand, playerHand, hiddenCard, dealerReveal);
+                System.out.println("Blackjack! You win.");
+                Chips.playerChips += (Chips.playerBet * (1.5));
+                break;
+            }
+
+            Deck.getStatus(dealerHand, playerHand, hiddenCard, dealerReveal);
+            int choice = Main.getInput("1 - Hit   |   2 - Stay   > ", new ArrayList<Integer>(Arrays.asList(1, 2)));
+            if (choice == 1) {
+                playerHand.add(Deck.drawCard(playDeck));
+            } else if (choice == 2) {
+                dealerReveal = true;
+                break;
             }
         }
-        Collections.shuffle(newDeck);
-        return newDeck;
-    }
 
-    public static String drawCard(ArrayList<String> deck) {
-        return deck.remove(0);
-    }
-
-    public static int getCardValue(String card) {
-        String cardRank = card.split(" ")[0];
-        int value = 0;
-        if ("JackQueenKing".contains(cardRank)) {
-            value = 10;
-        } else if (cardRank.equals("Ace")) {
-            value = 11;
-        } else {
-            value = values.get(cardRank);
-        }
-        return value;
-    }
-
-    public static int getHandValue(ArrayList<String> deck) {
-        int handValue = 0;
-        int aceAmount = 0;
-        for (String card : deck) {
-            handValue += getCardValue(card);
-            if (card.contains("Ace")) {
-                aceAmount++;
-            }
-        }
-        if (handValue > 21) {
-            while (aceAmount > 0) {
-                handValue -= 10;
-                if (handValue == 21)
+        if (playerBust == false && Deck.getHandValue(dealerHand) < 17) {
+            while (true) {
+                dealerHand.add(Deck.drawCard(playDeck));
+                if (Deck.getHandValue(dealerHand) >= 17) {
                     break;
-                aceAmount--;
+                }
+            }
+            dealerReveal = true;
+        }
+
+        System.out.println("\n\n");
+        Deck.getStatus(dealerHand, playerHand, hiddenCard, dealerReveal);
+        System.out.println("\n");
+
+        if (playerBust == false) {
+            if (Deck.getHandValue(dealerHand) > 21) {
+                System.out.println("Dealer's hand went over 21. You win.");
+                Chips.playerChips += Chips.playerBet;
+            } else if (Deck.getHandValue(playerHand) > Deck.getHandValue(dealerHand)) {
+                System.out.println("You win.");
+                Chips.playerChips += Chips.playerBet;
+            } else if (Deck.getHandValue(playerHand) == Deck.getHandValue(dealerHand)) {
+                System.out.println("It's a tie.");
+            } else {
+                System.out.println("Dealer wins.");
+                Chips.playerChips -= Chips.playerBet;
             }
         }
-        return handValue;
-    }
 
-    public static void getStatus(ArrayList<String> dealer, ArrayList<String> player, String hiddenCard, Boolean reveal) {
-        System.out.println("\n");
-        ArrayList<String> dealerHiddenHand = dealer;
-        dealerHiddenHand.remove(hiddenCard);
-        if (reveal == false) {
-            System.out.println("Dealer's Hand > " + "[Hidden] " + dealerHiddenHand + " ("
-                    + getHandValue(dealerHiddenHand) + "pts)");
-            System.out.println("Player's Hand > " + player + " (" + getHandValue(player) + "pts)");
-        } else if (reveal == true) {
-            System.out.println("Dealer's Hand > " + dealer + " (" + getHandValue(dealer) + "pts)");
-            System.out.println("Player's Hand > " + player + " (" + getHandValue(player) + "pts)");
+        System.out.println("Game over.");
+        resetGame();
+
+        while (true) {
+            int replay = Main.getInput("1 - Return to main menu   |   2 - Exit the game   > ", new ArrayList<Integer>(Arrays.asList(1, 2)));
+            if (replay == 2) {
+                System.out.println("\nThank you for playing!\n");
+                System.exit(0);
+                break;
+            } else if (replay == 1) {
+                return;
+            }
         }
     }
 
-    public static String getCardImage(String card) {
-        String cardRank = card.split(" ")[0].toLowerCase();
-        String fileName = card.replaceAll(" ", "_").toLowerCase();
-        String path = "Blackjack/cards/" + cardRank + "/" + fileName + ".png";
-        return path;
+    public static void resetGame() {
+        for (String card : dealerHand) {
+            dealerHand.remove(card);
+        }
+        for (String card : playerHand) {
+            playerHand.remove(card);
+        }
+        playerBust = false;
+        dealerReveal = false;
     }
 }
